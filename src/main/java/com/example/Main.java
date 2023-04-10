9541b418048c21eb4e435ea14c177d930c912e00
morita20432 <morita20432@cs.saisoncard.co.jp>
2023-04-10 20:10:44 +0900

    山札を生成 最初のプレイヤーの手札を生成 最初のcpuの手札を生成
    2回目の変更時の手札の生成の下書き
    
    上記変更を行いました

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,33 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // ポーカー
+        //52枚のカードのリストを生成
+        List<Cards> cardsList = Cards.createCardsList();
+
+        //52枚からランダムに5枚選択し、ArrayListに格納する
+        List<Cards> playersHandsCardsList = new ArrayList<>(); //プレイヤーの手札を生成
+        GetRandomCards.getRandomCards(cardsList, 5, playersHandsCardsList); //cardsListは47枚になる。
+        System.out.println(playersHandsCardsList);
+        System.out.println(cardsList.size());
+        //47枚からランダムに5枚選択し、ArrayListに格納する
+        List<Cards> cpusHandsCardsList = new ArrayList<>(); //cpuの手札を生成
+        GetRandomCards.getRandomCards(cardsList, 5, cpusHandsCardsList); //cardsListは42枚になる。
+        System.out.println(cpusHandsCardsList);
+        System.out.println(cardsList.size());
+
+        //todo プレイヤーにランダム抽出された5枚のカードを表示させる
+
+        //todo 5枚のカードのうち、変えたいカードを選択させる
+
+        //todo 選択した数ぶん、変えたいカードを手札から削除する。
+
+        //todo 選択した数ぶん、山札からランダムに選んだカードを追加する
+        GetRandomCards.getRandomCards(cardsList, 1, playersHandsCardsList); // todo drawCardに選択したカードの枚数を保持した変数を入れる
+        GetRandomCards.getRandomCards(cardsList, 1, cpusHandsCardsList); // todo drawCardに選択したカードの枚数を保持した変数を入れる
+
     }
 }
