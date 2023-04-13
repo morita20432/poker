679ba01d94de3b756f1f321f83a803e4eea5fd92
morita20432 <morita20432@cs.saisoncard.co.jp>
2023-04-13 14:24:50 +0900

    ロイヤルストレートフラッシュ ストレートフラッシュ ストレート フラッシュ の判定メソッドを実装

--- src/main/java/com/example/Judge.java
+++ src/main/java/com/example/Judge.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -13,7 +14,7 @@ public class Judge {
      */
     public static HandsGrade checkHands(List<Cards> handsCardsList) {
 
-        if (royal(handsCardsList)) {
+        if (royalStraightFlush(handsCardsList)) {
             return HandsGrade.ROYALFLUSH;
         } else if (straightFlush(handsCardsList)) {
             return HandsGrade.STRAIGHTFLUSH;
@@ -36,7 +37,7 @@ public class Judge {
         }
     }
 
-    public static void royalFlush(List<Cards> handsCardsList) {
+    public static void royal(List<Cards> handsCardsList) {
         int cardNumIndex = 1;
         List<Integer> cardNumList = new ArrayList<>();
         List<Integer> straightList = new ArrayList<>();
@@ -67,15 +68,35 @@ public class Judge {
     /**
      * 役がロイヤルストレートフラッシュか判定するメソッドです。
      */
-    public static boolean royal(List<Cards> handsCardsList) {
-        return true;
+    public static boolean royalStraightFlush(List<Cards> handsCardsList) {
+        List<Integer> cardNumGradeList = new ArrayList<>();
+        for (Cards cards : handsCardsList) {
+            cardNumGradeList.add(cards.getCardNumGrade());
+        }
+        //カードの強さでsort
+        Collections.sort(cardNumGradeList);
+
+        if (cardNumGradeList.get(0) == 10) {
+            if (straight(handsCardsList)) {
+                return flush(handsCardsList);
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
     }
 
     /**
      * 役がストレートフラッシュか判定するメソッドです。
      */
     public static boolean straightFlush(List<Cards> handsCardsList) {
-        return true;
+        if (!royalStraightFlush(handsCardsList)) {
+            return flush(handsCardsList) && straight(handsCardsList);
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -96,12 +117,12 @@ public class Judge {
      * 役がフラッシュか判定するメソッドです。
      */
     public static boolean flush(List<Cards> handsCardsList) {
-        List<Integer> suitList = new ArrayList<>();
+        List<Integer> suitGradeList = new ArrayList<>();
         for (Cards cards : handsCardsList) {
-            suitList.add(cards.getSuitGrade());
+            suitGradeList.add(cards.getSuitGrade());
         }
         for (int i = 0; i < 4; i++) {
-            if (suitList.get(i) != suitList.get(i + 1)) {
+            if (suitGradeList.get(i) != suitGradeList.get(i + 1)) {
                 return false;
             }
         }
@@ -112,7 +133,25 @@ public class Judge {
      * 役がストレートか判定するメソッドです。
      */
     public static boolean straight(List<Cards> handsCardsList) {
+        List<Integer> cardNumGradeList = new ArrayList<>();
+        for (Cards cards : handsCardsList) {
+            cardNumGradeList.add(cards.getCardNumGrade());
+        }
+        //カードの強さでsort
+        Collections.sort(cardNumGradeList);
 
+        //sort後の最小gradeが1(2)かつ、最大grade13(A)場合
+        if (cardNumGradeList.get(0) == 1 && cardNumGradeList.get(4) == 13) {
+            //ACEのgradeを0に変更
+            cardNumGradeList.set(4, 0);
+            //再び昇順でsort
+            Collections.sort(cardNumGradeList);
+        }
+        for (int i = 0; i < 4; i++) {
+            if (cardNumGradeList.get(i) != cardNumGradeList.get(i + 1) - 1) {
+                return false;
+            }
+        }
         return true;
     }
 
@@ -169,6 +208,8 @@ public class Judge {
      * プレイヤーの役とcpuの役を比較するメソッドです
      * HandsGradeを引数で受け取って、その数値を比較します
      */
+    // todo 引数で手札のスート、カードの強さの値を受け取る必要あり
+    //  elseで同じ役の場合、数値比較、スート比較のロジックを書く必要がある
     public static void judgeHands(HandsGrade playerHandsGrade, HandsGrade cpuHandsGrade) {
         if (playerHandsGrade.getHandsGrade() > cpuHandsGrade.getHandsGrade()) {
             System.out.println("プレイヤーの勝利です");
